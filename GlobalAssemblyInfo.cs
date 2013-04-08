d9ce25fd99bbe846ddd577faed4aa6886c6536be
Steven Thuriot <steven@thuriot.be>
2013-04-08 19:01:07 +0200

    Updated assembly info

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -22,10 +22,10 @@ using System.Reflection;
 using System.Resources;
 
 [assembly: AssemblyCopyright("Thuriot.be")]
-[assembly: AssemblyDescription("Small graphical framework to quickly start developing your apps without having to worry too much about controls and looks.")]
+[assembly: AssemblyDescription("Nova Action Queuing System.")]
 [assembly: AssemblyCompany("Steven Thuriot")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyInformationalVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
